87a1e2d407b52a796157342e8d77c6579864c719
Lucas Dominguez <luhcas_@live.com>
2022-05-16 20:02:58 -0300

    galeria

--- js/script.js
+++ js/script.js
@@ -42,8 +42,6 @@ function ativarPergunta(event) {
 
     const ativa = resposta.classList.contains("ativa");
     pergunta.setAttribute('aria-expanded', ativa);
-
-    console.log(resposta);
 }
 
 function eventosPerguntas(pergunta) {
@@ -52,3 +50,24 @@ function eventosPerguntas(pergunta) {
 
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;
+    
+    if(media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+
+galeria.forEach(eventosGaleria);
